8e0ac67819a3dc320abb899797f03796976349c5
chegrofelix <chegerofelix@gmail.com>
2021-02-23 01:14:27 +0300

    created 3 new files aljazeera.html buzzfeed.html and bbc.html and modified 4 files

--- app/views.py
+++ app/views.py
@@ -1,11 +1,99 @@
 from flask import render_template
 from app import app
+from newsapi import NewsApiClient
 
-# Views
+
+
+
 @app.route('/')
 def index():
+    newsapi = NewsApiClient(api_key="c7414509761344ce8321cc7ae296ade4")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news")
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    articles = topheadlines['articles']
+    
+    
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+        
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+    mylist = zip(news, desc, img)
+
+
+
+    return render_template('index.html', context= mylist)
+
+
+@app.route('/bbc')
+def bbc():
+    newsapi = NewsApiClient(api_key="c7414509761344ce8321cc7ae296ade4")
+    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+    mylist = zip(news, desc, img)
+ 
+    return render_template('bbc.html', context=mylist)
+
+@app.route('/buzzfeed')
+def buzzfeed():
+    newsapi = NewsApiClient(api_key="c7414509761344ce8321cc7ae296ade4")
+    topheadlines = newsapi.get_top_headlines(sources="Buzzfeed")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+    mylist = zip(news, desc, img)
+ 
+    return render_template('buzzfeed.html', context=mylist)
+
+@app.route('/aljazeera')
+def aljazeera():
+    newsapi = NewsApiClient(api_key="c7414509761344ce8321cc7ae296ade4")
+    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+ 
+    articles = topheadlines['articles']
+ 
+    desc = []
+    news = []
+    img = []
+ 
+    for i in range(len(articles)):
+        myarticles = articles[i]
+ 
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+ 
+    mylist = zip(news, desc, img)
+ 
+    return render_template('aljazeera.html', context=mylist)
