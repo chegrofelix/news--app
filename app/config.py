aa703051d5e4cdd2de0a71b9d69a5ff503ae03a7
chegrofelix <chegerofelix@gmail.com>
2021-02-23 12:20:01 +0300

    modified index.html and renamed ,env to .env

--- app/config.py
+++ app/config.py
@@ -1,2 +1,34 @@
 import os
 
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    CSRF_ENABLED = os.environ.get('CSRF_ENABLED')
+   
+
+
+
+
+class ProductionConfig(Config):
+    DEBUG = os.environ.get("DEBUG")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = os.environ.get('DEBUG')
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+config_options = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing':TestingConfig,
+
+
+}
+
